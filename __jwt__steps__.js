d20f1d5355a1f3050e6743bd33e72d863c4ff2ae
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-20 03:04:10 +0600

    bookings cookie

--- __jwt__steps__.js
+++ __jwt__steps__.js
@@ -4,3 +4,23 @@
     * token client
 */
 
+/* 
+    * how to store token in the client side
+    1. memory --> ok type
+    2. local storage --> ok type (xss)
+    3. cookies : http only
+
+*/
+
+/* 
+    1. set cookie with https only. for development secure: false,
+
+    2. cors
+        app.use(cors({
+            origin: ['http://localhost:5173'],
+            credentials: true
+            }));
+
+    3. client side axios settings
+      -> in axios set withCredentials: true 
+*/
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdflpzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,7 +40,14 @@ async function run() {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.send(token)
+
+      
+      res
+      .cookie('token', token,{
+        httpOnly: true,
+        secure: false,
+      })
+      .send({success: true})
     })
 
 
@@ -68,7 +80,8 @@ async function run() {
     })
 
     app.get('/bookings', async(req, res)=>{
-      //console.log(req.query.email);
+      console.log(req.query.email);
+      console.log('tok tok token', req.cookies.token);
       let query = {}
       if(req.query?.email){
         query={email: req.query.email}
